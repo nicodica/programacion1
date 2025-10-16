035920a2409734f992f342b6964c8fcf238b23c1
Diana Callejas <diana.callejas@ucb.edu.bo>
2025-10-16 13:45:58 -0400

    ejercicio 1 arreglado

--- practica_7/ejercicio_07_01.cpp
+++ practica_7/ejercicio_07_01.cpp
@@ -16,12 +16,11 @@ int main () {
     system("cls");
 
     int matriz[100][100];
-    int aux=0; //Entero auxiliar para realizar el intermabio de valores en las filas
-    int n; //Matriz cuadrada, es igual el número de filas y columnas
+    int aux=0; //Entero auxiliar para realizar el intercambio de valores en las filas
+    int n; //Matriz cuadrada: es igual el número de filas y columnas
     
     cout << "Introduce el número de filas y columnas (nxn): ";
     cin >> n;
-    // int matriz[n][n];
 
     // Llenar la matriz
     for (int i=0; i<n; i++) {
@@ -33,37 +32,20 @@ int main () {
         cout << endl;
     }
 
-    // Imprimir la matriz
-    /* for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            cout << matriz[i][j] << "\t";
-        }
-        cout << endl;
-    } */
-
     imprimirMatriz(matriz, n, n);
+    cout << endl;
 
-    // Cambiar la primera y ultima fila (o sea i)
+    // Cambiar la primera y ultima fila
     for (int i=0; i<n; i++) {
         aux = matriz[0][i]; // Guardar en una auxliar un valor de la primera fila
         matriz[0][i] = matriz[n-1][i]; //Intercambiar el valor de la primera con la última
         matriz[n-1][i] = aux; //Rescatar el valor del auxliar
     }
-
-    // Imprimir la matriz modificada
-    /* cout << endl;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            cout << matriz[i][j] << "\t";
-        }
-        cout << endl;
-    } */
-   imprimirMatriz(matriz, n, n);
+    // Imprimir la matriz ya cambiada
+    imprimirMatriz(matriz, n, n);
 
     return 0;
 }
-
-// Investiga como realizar funciones con matrices
 
 void imprimirMatriz (int matriz[100][100], int filas, int columnas) {
     for (int i=0; i<filas; i++) {
